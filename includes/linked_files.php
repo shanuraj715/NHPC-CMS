3ef9fb9eb6fda5e6b4fccc6b98508e53a07d541c
Shanu Raj <shanuraj715@gmail.com>
2021-05-20 03:01:31 +0530

    FA Removed

--- includes/linked_files.php
+++ includes/linked_files.php
@@ -5,10 +5,8 @@
 <link rel="stylesheet" href="<?php echo $base_url;?>/css/sidebar.css">
 <link rel="stylesheet" href="<?php echo $base_url;?>/css/post_block.css">
 <link rel="stylesheet" href="<?php echo $base_url;?>/css/view-post.css">
-<link rel="stylesheet" href="<?php echo $base_url;?>/css/fonts-awesome/css/all.css">
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<link rel="stylesheet" href="https://projects.techfacts007.in/fontawesome/css/all.min.css">
 <script type="text/javascript" src="<?php echo $base_url;?>/js/date_time.js"></script>
 <script type="text/javascript" src="<?php echo $base_url;?>/js/navbar_functions.js"></script>
 <link rel="icon" href="<?php echo $base_url;?>/images/fevicon.png">
-<!-- <script src="https://kit.fontawesome.com/49d14b2c4c.js"></script> -->
 <?php include './functions/search_functions.php';?>
